270d82ac560faf5760d652fe4c064bb1bada1316
Brian <bmvukwe.brian.mvukwe11@gmail.com>
2025-08-15 21:28:22 +0200

    making improvements

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CreateEvent from "./pages/create_event";
 import EventList from "./pages/events";
-import RSVP from "./pages/rsvp";
+// import RSVP from "./pages/rsvp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<EventList />} />
       <Route path="/create" element={<CreateEvent />} />
-      <Route path="/rsvp/:event_id" element={<RSVP />} />
+      {/* <Route path="/rsvp/:event_id" element={<RSVP />} /> */}
     </Routes>
   </BrowserRouter>
 );
